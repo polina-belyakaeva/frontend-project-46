957ced93843e46c08db7c9ee582f358936b1ba31
Полина Белякаева <polinabelakaeva@MBP-Polina.modem.local>
2023-03-15 16:51:15 +0300

    Add arguments and option to gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,5 +5,8 @@ const program = new Command();
 program
   .version('0.0.0')
   .description('Compares two configuration files and shows a difference')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
