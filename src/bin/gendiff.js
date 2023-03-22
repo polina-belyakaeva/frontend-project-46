9cf4ecd827f25d8e552042df7649e0119147a9ce
Полина Белякаева <polinabelakaeva@MBP-Polina.modem.local>
2023-03-22 17:26:00 +0300

    Add genDiff function

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { genDiff } from '../genDiff.js';
 const program = new Command();
 
 program
@@ -8,5 +9,8 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2)); 
+  });
 
 program.parse();
