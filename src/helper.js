5d2da5339fce535850c1a0e9569355e6583aa714
Полина Белякаева <polinabelakaeva@MBP-Polina.modem.local>
2023-03-31 15:36:50 +0300

    changed tests

---
+++ src/helper.js
@@ -0,0 +1,14 @@
+import path from 'path';
+import { readFileSync } from 'fs';
+
+const getDir = (filepath) => path.dirname(filepath);
+
+const readFile = (filepath) => readFileSync(filepath, 'utf-8');
+
+const getExtension = (filepath) => path.extname(filepath);
+
+export {
+  getDir,
+  readFile,
+  getExtension,
+};
