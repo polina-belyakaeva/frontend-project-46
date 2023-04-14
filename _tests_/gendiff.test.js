e4cf493d15e0a26d68a341b4fba1315c0dd9c992
Полина Белякаева <polinabelakaeva@MBP-Polina.modem.local>
2023-04-14 11:56:42 +0300

    add gendiff for resursive types

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -1,23 +1,68 @@
 import { test, expect } from '@jest/globals';
-// import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { getDir } from '../src/helper.js';
-import { genDiff } from '../src/genDiff.js';
+import { genDiff } from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = getDir(__filename);
+
+const expectedOutput = 
+`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
 
 test('JSON test', () => {
     const path1 = path.resolve(__dirname, '../__fixtures__', 'file1.json');
     const path2 = path.resolve(__dirname, '../__fixtures__', 'file2.json');
 
-    expect(genDiff(path1, path2)).toEqual('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+    expect(genDiff(path1, path2)).toEqual(expectedOutput);
 });
 
 test('YAML test', () => {
     const path1 = path.resolve(__dirname, '../__fixtures__', 'file1.yml');
     const path2 = path.resolve(__dirname, '../__fixtures__', 'file2.yml');
 
-    expect(genDiff(path1, path2)).toEqual('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+    expect(genDiff(path1, path2)).toEqual(expectedOutput);
 });
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { genDiff } from '../genDiff.js';
+import { genDiff } from '../index.js';
+
 const program = new Command();
 
 program
@@ -8,7 +9,7 @@ program
   .description('Compares two configuration files and shows a difference')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2)); 
   });
